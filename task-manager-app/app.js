c2c0824e7d589ae34a9341626914b6f40afbbadc
Javierg9n4 <javierg9n4@gmail.com>
2022-11-19 13:41:26 +0100

    clean up some comments

--- task-manager-app/app.js
+++ task-manager-app/app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+
 //middleware
 app.use(express.static('./public'))
 app.use(express.json());
@@ -11,13 +12,6 @@ app.use(express.json());
 // routes
 app.use('/api/v1/tasks', tasks);
 
-
-/* app.get('/api/v1/tasks') // get all the tasks
-app.post('/api/v1/tasks') // create a new task
-app.get('/api/v1/tasks/:id') // get a single task by id
-app.patch('/api/v1/tasks/:id') // update some data of a single task
-app.delete('/api/v1/tasks/:id') // delete a single task
- */
 
 const port = 3000
 
